3046421915604f211312024f8bd334af9d1dd39a
uni4x <micro09188@gmail.com>
2024-08-01 14:10:10 +0900

    Add render domain to ALLOWED_HOSTS

--- news_aggregator/settings.py
+++ news_aggregator/settings.py
@@ -40,7 +40,8 @@ SECRET_KEY = "django-insecure-r7&#@o^op4jfm#(7@jr)3ofa@1)(nmhe5j_pt=xxg6bblrthki
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['repo4-cw1u.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
